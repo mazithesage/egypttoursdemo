9c3231c8545cace71c0af4eda55d2bafa5427942
royalManuel <104584134+EmmanuelVictor62@users.noreply.github.com>
2024-11-04 00:30:53 +0100

    fix: Fixed issue with loader display on production

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,17 @@
-const ham = document.querySelector('.ham-menu')
-const nav = document.querySelector('.navbar')
+const ham = document.querySelector(".ham-menu");
+const nav = document.querySelector(".navbar");
 
-ham.addEventListener("click",()=>{
-    ham.classList.toggle('active')
-    nav.classList.toggle('active')
-})
-
-const loadder = document.getElementById('preloadder')
-
-window.addEventListener("load",()=>{
-    loadder.style.display = "none";
+ham.addEventListener("click", () => {
+  ham.classList.toggle("active");
+  nav.classList.toggle("active");
 });
+
+const loadder = document.getElementById("preloadder");
+
+window.addEventListener("load", () => {
+  loadder.style.display = "none";
+});
+
+setTimeout(() => {
+  loadder.style.display = "none";
+}, 5000);
