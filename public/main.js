92172c1c8417b5ddfa924a4f9ba8b18f5c905ef1
royalManuel <104584134+EmmanuelVictor62@users.noreply.github.com>
2024-11-04 02:46:08 +0100

    feat: Can now toggle nav with nav buttons

--- public/main.js
+++ public/main.js
@@ -1,10 +1,17 @@
 const ham = document.querySelector(".ham-menu");
 const nav = document.querySelector(".navbar");
-const closeBtn = document.querySelector(".toggle-button");
+const navBtn = document.querySelectorAll(".navbar-items");
 
 ham.addEventListener("click", () => {
   ham.classList.toggle("active");
   nav.classList.toggle("active");
+});
+
+navBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    ham.classList.toggle("active");
+    nav.classList.toggle("active");
+  });
 });
 
 const loadder = document.getElementById("preloadder");
